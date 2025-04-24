e47e94eeb43cc7bca80b0f74915c5ef1c4feae54
paulo-paim-dev <139926189+paulo-paim-dev@users.noreply.github.com>
2025-04-24 13:27:28 -0300

    Ajusta jogo facil, background e responsividade de tela.

--- js/jogoMemoria _facil.js
+++ js/jogoMemoria _facil.js
@@ -4,28 +4,28 @@ const timer = document.querySelector(".timer");
 
 const conteudos = [
   "facil1",
-  "resp-facil1",
-  "facil(2)",
-  "resp-facil(2)",
-  "resp-facil(3)",
-  "facil(3)",
-  "resp-facil(4)",
-  "facil(4)",
-  "resp-facil(5)",
-  "facil(5)",
+  "resp-facil-1",
+  "facil2",
+  "resp-facil-2",
+  "facil3",
+  "resp-facil-3",
+  "facil4",
+  "resp-facil-4",
+  "facil5",
+  "resp-facil-5",
 ];
 
 const pares = {
-  facil1: "resp-facil1",
-  "resp-facil1": "facil1",
-  "facil(2)": "resp-facil(2)",
-  "resp-facil(2)": "facil(2)",
-  "resp-facil(3)": "facil(3)",
-  "facil(3)": "resp-facil(3)",
-  "resp-facil(4)": "facil(4)",
-  "facil(4)": "resp-facil(4)",
-  "resp-facil(5)": "facil(5)",
-  "facil(5)": "resp-facil(5)",
+  "facil1": "resp-facil-1",
+  "resp-facil-1": "facil1",
+  "facil2": "resp-facil-2",
+  "resp-facil-2": "facil2",
+  "facil3": "resp-facil-3",
+  "resp-facil-3": "facil3",
+  "facil4": "resp-facil-4",
+  "resp-facil-4": "facil4",
+  "facil5": "resp-facil-5",
+  "resp-facil-5": "facil5",
 };
 
 const criarElemento = (tag, classe) => {
@@ -39,7 +39,7 @@ let segundaCarta = "";
 
 const checarVencedor = () => {
   const cartasDesabilitadas = document.querySelectorAll(".desativar-carta");
-  if (cartasDesabilitadas.length === conteudos.length * 2) {
+  if (cartasDesabilitadas.length === conteudos.length) {
     clearInterval(this.loop);
     alert(
       `Parabéns ${localStorage.getItem("jogador")}! Você ganhou em ${
@@ -72,17 +72,29 @@ const verificaCartas = () => {
   }
 };
 
-const revelaCarta = ({ target }) => {
-  if (target.parentNode.classList.contains("revela-carta")) {
+const revelaCarta = (evento) => {
+  let card = evento.target;
+
+  while (!card.classList.contains("card")) {
+    card = card.parentNode;
+    if (!card) {
+      return;
+    }
+  }
+
+  if (
+    card.classList.contains("revela-carta") ||
+    card.firstChild.classList.contains("desativar-carta")
+  ) {
     return;
   }
 
   if (primeiraCarta === "") {
-    target.parentNode.classList.add("revela-carta");
-    primeiraCarta = target.parentNode;
+    card.classList.add("revela-carta");
+    primeiraCarta = card;
   } else if (segundaCarta === "") {
-    target.parentNode.classList.add("revela-carta");
-    segundaCarta = target.parentNode;
+    card.classList.add("revela-carta");
+    segundaCarta = card;
     verificaCartas();
   }
 };
@@ -92,7 +104,7 @@ const criarCard = (conteudo) => {
   const frente = criarElemento("div", "face frente");
   const verso = criarElemento("div", "face verso");
 
-  frente.style.backgroundImage = `url(./imagens/${conteudo}.png)`;
+  frente.style.backgroundImage = `url(../images/${conteudo}.png)`;
 
   card.appendChild(frente);
   card.appendChild(verso);
